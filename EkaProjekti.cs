3afcf14a656b95096e565f63493e11d2ad6d8e07
Sisu Saastamoinen <Sisu.Saastamoinen@edu.keuda.fi>
2026-01-19 10:51:13 +0200

    fixed stuff

--- EkaProjekti.cs
+++ EkaProjekti.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace HelloWorld {
     class Program {
-        static void Main(string[], args) {
+        static void Main(string[] args) {
             Console.WriteLine("Hello world!");
         }
     }
